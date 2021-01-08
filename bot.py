9b6f20501ae00a0e1bb5479fb8cf8dac8a0063c6
Jason Chen <57494592+picklejason@users.noreply.github.com>
2021-01-08 14:31:50 -0500

    Add ELO history graph on reaction

--- bot.py
+++ bot.py
@@ -5,13 +5,18 @@ import asyncio
 import os
 import sys
 import re
+import io
 import aiohttp
 import asyncio
 import json
 import traceback
 import discord
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from matplotlib.collections import LineCollection
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -42,6 +47,7 @@ async def process_stats(author, num_matches=3):
     user_id = users[author]['user_id']
     with open('headers.json', 'r') as data:
         headers = json.load(data)['headers']
+
     after, diff, rank_nums, maps, arrows, start_times, prev_matches = await rs.get_stats(user_id, headers, num_matches)
     rank_num = rank_nums[0]
     stats = zip(diff, maps, arrows, start_times)
@@ -52,6 +58,7 @@ async def process_stats(author, num_matches=3):
 
     with open('info.json', 'w') as f:
         json.dump(users, f, indent=4)
+
     return stats, rank_num, rank, RP, ELO
 
 # Log in using RSO_AuthFlow and links discord ID with player ID
@@ -150,7 +157,7 @@ async def send_update():
                 discord_user = await bot.fetch_user(user)
 
                 if await rs.check(user_id, headers, prev_matches):
-                    return
+                    pass
                 else:
                     await discord_user.send(embed = await create_embed(track_user))
             else:
@@ -183,11 +190,30 @@ async def untrack(ctx):
 async def profile(ctx, user : discord.User = ''):
     try:
         if not user:
-            user = str(ctx.author.id)
+            user_id = str(ctx.author.id)
         else:
-            user = str(user.id)
+            user_id = str(user.id)
 
-        await ctx.send(embed = await create_embed(user))
+        embed = await create_embed(user_id)
+        msg = await ctx.send(embed=embed)
+
+        await msg.add_reaction('📈')
+        def check(reaction, user):
+            return user == ctx.message.author and str(reaction.emoji) == '📈'
+
+        while True:
+            try:
+                reaction, user = await bot.wait_for('reaction_add', check=check, timeout=120)
+            except asyncio.TimeoutError:
+                await msg.remove_reaction('📈', bot.user)
+                break
+
+            image = await graph(user_id)
+            # embed_ = await create_embed(user_id)
+            embed.set_image(url=f'attachment://graph.png')
+            await msg.delete()
+            await ctx.send(file=image, embed=embed)
+            break
 
     except Exception as e:
         print(traceback.format_exc())
@@ -211,6 +237,64 @@ async def create_embed(discord_id):
         embed.add_field(name=f'{match_map} ∙ {start_time}', value=f'{movement} {stat}', inline=False)
 
     return embed
+
+async def graph(user):
+    num_matches = 20
+    with open ('info.json', 'r') as f:
+        users = json.load(f)
+
+    user_id = users[user]['user_id']
+    with open('headers.json', 'r') as data:
+        headers = json.load(data)['headers']
+
+    after, diff, rank_nums, maps, arrows, start_times, prev_matches = await rs.get_stats(user_id, headers, num_matches)
+    ELO = list(reversed([(rank_num * 100) - 300 + RP for RP, rank_num in zip(after, rank_nums)]))
+
+    x = np.arange(len(ELO))
+    y = np.array(ELO)
+
+    segments_x = np.r_[x[0], x[1:-1].repeat(2), x[-1]].reshape(-1, 2)
+    segments_y = np.r_[y[0], y[1:-1].repeat(2), y[-1]].reshape(-1, 2)
+
+    # Assign colors to the line segments
+    linecolors = ['green' if y_[0] < y_[1] else 'red'
+                  for y_ in segments_y]
+
+    segments = [list(zip(x_+1, y_)) for x_, y_ in zip(segments_x, segments_y)]
+    min_ = int(math.floor(min(ELO) / 10.0)) * 10
+    max_ = int(math.ceil(max(ELO) / 10.0)) * 10
+    # Create figure
+    plt.figure(figsize=(12,5), dpi=150)
+    plt.style.use('dark_background')
+    ax = plt.axes()
+
+    # Add a collection of lines
+    ax.add_collection(LineCollection(segments, colors=linecolors))
+    ax.scatter([x_ + 1 for x_ in x], y, c=[linecolors[0]]+linecolors, zorder=10)
+    ax.set_xlim(0, len(x)+1)
+    ax.set_ylim(min_, max_)
+
+    ax.xaxis.grid(linestyle='dashed')
+    ax.yaxis.grid(linestyle='dashed')
+    ax.spines['top'].set_linestyle('dashed')
+    ax.spines['bottom'].set_capstyle('butt')
+    ax.spines['right'].set_linestyle('dashed')
+    ax.spines['bottom'].set_capstyle('butt')
+    plt.xlabel('Past Matches')
+    plt.ylabel('ELO')
+    plt.title('ELO History')
+    plt.xticks(np.arange(0, len(x)+1))
+    plt.yticks(np.arange(min_, max_, 100))
+
+    plt.tight_layout()
+    plt.savefig('graph.png', transparent=True)
+    plt.close()
+
+    with open('graph.png', 'rb') as f:
+        file = io.BytesIO(f.read())
+    image = discord.File(file, filename='graph.png')
+
+    return image
 
 if __name__ == '__main__':
     bot.run(TOKEN)
